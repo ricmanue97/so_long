55e8fea9bccaa73f98814ec18fcf9e2d4a2d2ad8
Ricardo Manuel Ribeiro Alcobia <ricmanue@c2r5s9.42lisboa.com>
2024-09-19 11:59:14 +0100

    Finalizing ft_game_init, with onitialization of images

--- so_long/game_init.c
+++ so_long/game_init.c
@@ -6,19 +6,21 @@
 /*   By: ricmanue <ricmanue@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 13:44:53 by ricmanue          #+#    #+#             */
-/*   Updated: 2024/09/18 16:22:26 by ricmanue         ###   ########.fr       */
+/*   Updated: 2024/09/19 11:57:48 by ricmanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_which_sprite(t_game *game)
+void	ft_image_init(t_game *game)
 {
-
-
+	ft_which_sprite(game, &game->player->img_player, "./test1.xpm");
+	ft_which_sprite(game, &game->map->img_floor1, "./floor1.xpm");
+	ft_which_sprite(game, &game->map->img_floor2, "./floor2.xpm");
+	ft_which_sprite(game, &game->map->img_wall, "./wall.xpm");
 }
 
-void	ft_img_init(t_game *game, void **img, char *path)
+void	ft_which_sprite(t_game *game, void **img, char *path)
 {
 	int	img_width;
 	int	img_height;
@@ -34,5 +36,8 @@ void	ft_game_init(t_game *game)
 	if (!game->mlx)
 		ft_error_management("Error: Unable to start mlx", game);
 	game->player->moves = 0;
-
+	ft_image_init(game);
+	game->win = mlx_new_window(game->mlx, game->map->map_width, game->map->map_height, "Catch them all!!");
+	if (!(game->win))
+		ft_error_management("Error: Could not open window", game);
 }
