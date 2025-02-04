c320e1dae0368a99546ad88d216cff56450d1eec
Ricardo Manuel Ribeiro Alcobia <ricmanue@c2r6s10.42lisboa.com>
2025-02-04 15:53:13 +0000

    adding map_opening and subsquent functions

---
+++ so_long/map_opening.c
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_opening.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ricmanue <ricmanue@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/04 14:25:22 by ricmanue          #+#    #+#             */
+/*   Updated: 2025/02/04 15:43:00 by ricmanue         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+static void	ft_map_height(t_game *game)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(game->map->path, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_printf("Error opening file\n");
+		exit(1);
+	}
+	game->map->map_height = 0;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break;
+		free(line);
+		game->map->map_height++;
+	}
+	close(fd);
+
+}
+static void	ft_map_ber(t_game *game, int fd)
+{
+	int		i;
+	char	*line;
+	char	**map;
+
+	map = malloc(sizeof(char *) * (game->map->map_height + 1));
+	if (!map)
+	{
+		ft_printf("Errror : malloc of map failed");
+		exit(1);
+	}
+
+	map[game->map->map_height] = NULL;
+	i = 0;
+	while (i < game->map->map_height)
+	{
+		line = get_next_line(fd);
+		map[i] = ft_substr(line, 0, ft_strlen(line));
+		free(line);
+		i++;
+	}
+	game->map->map_ber = map;
+}
+
+
+void	ft_map_creation(t_game *game)
+{
+	int	fd;
+
+	fd = open(game->map->path, O_RDONLY);
+	if (fd < 0)
+	{
+		ft_printf("Error opening file\n");
+		exit(1);
+	}
+	ft_map_height(game);
+	ft_map_ber(game, fd);
+	close(fd);
+}
