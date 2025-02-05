62961337eaa2111f9dfdc180822d9e1ac73318ac
Ricardo Manuel Ribeiro Alcobia <ricmanue@c2r6s10.42lisboa.com>
2025-02-05 17:00:17 +0000

    correcao da width em wall check

--- so_long/map_check.c
+++ so_long/map_check.c
@@ -6,15 +6,15 @@
 /*   By: ricmanue <ricmanue@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 11:09:13 by ricmanue          #+#    #+#             */
-/*   Updated: 2025/02/04 16:40:47 by ricmanue         ###   ########.fr       */
+/*   Updated: 2025/02/05 16:59:51 by ricmanue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	ft_file_check (char *file, t_game *game)
+void	ft_file_check(char *file, t_game *game)
 {
-	size_t len;
+	size_t	len;
 
 	len = ft_strlen(file);
 	if (len < 4 || ft_strncmp((&file[len - 4]), ".ber", 4) != 0)
@@ -25,25 +25,25 @@ void	ft_file_check (char *file, t_game *game)
 	}
 }
 
-void	ft_wall_check (t_game *game)
+void	ft_wall_check(t_game *game)
 {
-	int i;
+	int	i;
 
 	i = 0;
-		ft_printf("ola\n");
 	while (i < game->map->map_width)
 	{
-		if ((game->map->map_ber[0][i] == 1)
-			&& game->map->map_ber[game->map->map_height - 1][i] == 1)
+		if ((game->map->map_ber[0][i] == '1')
+			&& game->map->map_ber[game->map->map_height][i] == '1')
 			i++;
 		else
 			ft_error_management("Error : map frame\n", game);
 	}
 	i = 1;
-	while (i < game->map->map_height - 1)
+	while (i < game->map->map_height)
 	{
-		if ((game->map->map_ber[i][0] == 1)
-			&& game->map->map_ber[game->map->map_width - 1][i] == 1)
+		ft_printf("ola\n");
+		if ((game->map->map_ber[i][0] == '1')
+			&& game->map->map_ber[i][game->map->map_width] == '1')
 			i++;
 		else
 			ft_error_management("Error : map frame\n", game);
@@ -73,16 +73,17 @@ void	ft_sprite_limit_check(t_game *game, int i, int j)
 		j++;
 	}
 }
+
 void	ft_map_sprite_check(t_game *game)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < game->map->map_height)
 	{
 		ft_sprite_limit_check(game, i, 0);
 		if ((game->player_count > 1) || (game->exit > 1))
-		ft_error_management("Error : too many players or exits\n", game);
+			ft_error_management("Error : too many players or exits\n", game);
 		i++;
 	}
 }
@@ -91,22 +92,21 @@ void	ft_map_check(t_game *game)
 {
 	int	i;
 	int	len;
-	int	len_tmp;
-
 
 	if (game->map->map_ber[0] == NULL)
 		ft_error_management("Error : map file is empty\n", game);
 	i = 0;
-	len = ft_strlen(game->map->map_ber[0]);
+	len = ft_strlen(game->map->map_ber[0]) - 1;
+	game->map->map_width = len;
 	while (i < game->map->map_height)
 	{
-		len_tmp = ft_strlen(game->map->map_ber[i]);
-		if (len != len_tmp)
-		ft_error_management("Error : map is not rectangular\n", game);
+		len = ft_strlen(game->map->map_ber[i]) - 1;
+		if (len != game->map->map_width)
+			ft_error_management("Error : map is not rectangular\n", game);
 		i++;
 	}
+	game->map->map_width--;
 	ft_wall_check(game);
 	ft_map_sprite_check(game);
 	ft_valid_map(game);
 }
-
